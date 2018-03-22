8397541e33ed25b11e3f4a4f4afde6c4358327ab
Sean Moakes <seanmoakes@hotmail.com>
2018-03-22 10:44:06 +0000

    Made system from the input portion of PlayerInput

--- Assets/GameCode/PlayerInputSystem.cs
+++ Assets/GameCode/PlayerInputSystem.cs
@@ -1,7 +1,35 @@
-﻿namespace TwoStickClassicExample
+﻿using Unity.Entities;
+using UnityEngine;
+
+namespace TwoStickClassicExample
 {
-    public class PlayerInputSystem
+    public class PlayerInputSystem : ComponentSystem
     {
-        
+        struct PlayerData
+        {
+            public PlayerInput Input;
+        }
+        // No need for code injection when simply looping through entities
+
+        protected override void OnUpdate()
+        {   
+            // Get Time.deltaTime outside of the loop to prevent multiple calls
+            float dt = Time.deltaTime;
+
+            // GetEntities<T>() gives us the ComponentGroup matching the components
+            // in struct 'T'
+            foreach (var entity in GetEntities<PlayerData>())
+            {
+                var pi = entity.Input;
+
+                // From PlayerInput's update method
+                pi.Move.x = Input.GetAxis("Horizontal");
+                pi.Move.y = Input.GetAxis("Vertical");
+                pi.Shoot.x = Input.GetAxis("ShootX");
+                pi.Shoot.y = Input.GetAxis("ShootY");
+                
+                pi.FireCooldown = Mathf.Max(0.0f, pi.FireCooldown - dt);
+            }
+        }
     }
 }
