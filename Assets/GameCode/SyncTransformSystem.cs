7b36ef9e3826c9a80ee46f9a9061e5ccf5eaf32b
Sean Moakes <seanmoakes@hotmail.com>
2018-03-22 10:20:13 +0000

    SyncTransformSystem 0.1

--- Assets/GameCode/SyncTransformSystem.cs
+++ Assets/GameCode/SyncTransformSystem.cs
@@ -1,7 +1,30 @@
-﻿namespace TwoStickClassicExample
+﻿using Unity.Collections;
+using Unity.Entities;
+using Unity.Mathematics;
+using UnityEngine;
+
+namespace TwoStickClassicExample
 {
-    public class SyncTransformSystem
+    public class SyncTransformSystem : ComponentSystem
     {
-        
+        public struct Data
+        {
+
+            [ReadOnly] public Transform2D Transform;
+            public Transform Output;
+        }
+
+        protected override void OnUpdate()
+        {
+            foreach (var entity in GetEntities<Data>())
+            {
+
+                float2 p = entity.Transform.Position;
+                float2 h = entity.Transform.Heading;
+                entity.Output.position = new float3(p.x, 0, p.y);
+                if (!h.Equals(new float2(0f, 0f)))
+                    entity.Output.rotation = Quaternion.LookRotation(new float3(h.x, 0f, h.y), new float3(0f, 1f, 0f));
+            }
+        }
     }
 }
