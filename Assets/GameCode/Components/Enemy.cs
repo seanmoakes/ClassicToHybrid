c915208c91d3a10b7cdfedbcd29c269efb831480
Sean Moakes <seanmoakes@hotmail.com>
2018-03-26 18:58:04 +0100

    again

--- Assets/GameCode/Components/Enemy.cs
+++ Assets/GameCode/Components/Enemy.cs
@@ -6,20 +6,6 @@ namespace TwoStickClassicExample
 
     public class Enemy : MonoBehaviour
     {
-        //private void Update()
-        //{
 
-        //    var player = FindObjectOfType<PlayerInput>();
-
-        //    var settings = TwoStickBootstrap.Settings;
-        //    var minY = settings.playfield.yMin;
-        //    var maxY = settings.playfield.yMax;
-
-        //    if (GetComponent<Position2D>().Value.y > maxY || GetComponent<Position2D>().Value.y < minY)
-        //    {
-        //        GetComponent<Health>().Value = -1;
-        //    }
-            
-        //}
     }
 }
