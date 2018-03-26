00721766025dca07cf19b3c6fe0502f1e701a0f8
Sean Moakes <seanmoakes@hotmail.com>
2018-03-26 11:44:44 +0100

    Successful Test, remove behaviour

--- Assets/GameCode/Components/PlayerInput.cs
+++ Assets/GameCode/Components/PlayerInput.cs
@@ -11,28 +11,5 @@ namespace TwoStickClassicExample
         [HideInInspector] public float FireCooldown;
 
         public bool Fire => FireCooldown <= 0.0 && math.length(Shoot) > 0.5f;
-
-        //private void Update()
-        //{            
-        //    var settings = TwoStickBootstrap.Settings;
-
-        //    GetComponent<Position2D>().Value += Time.deltaTime * Move * settings.playerMoveSpeed;
-
-        //    if (Fire)
-        //    {
-        //        GetComponent<Heading2D>().Value = math.normalize(Shoot);
-
-        //        FireCooldown = settings.playerFireCoolDown;
-                
-        //        var newShotData = new ShotSpawnData()
-        //        {
-        //            Position = GetComponent<Position2D>().Value,
-        //            Heading = GetComponent<Heading2D>().Value,
-        //            Faction = GetComponent<Faction>()
-        //        };
-                
-        //        ShotSpawnSystem.SpawnShot(newShotData);
-        //    }
-        //}
     }
 }
