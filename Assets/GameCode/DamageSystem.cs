2d8dc6a8268847c1dd5e6bc6b839dc859e0aebd7
Sean Moakes <seanmoakes@hotmail.com>
2018-03-26 19:03:05 +0100

    Create DamageSystem.cs to house PlayerDamageSystem

---
+++ Assets/GameCode/DamageSystem.cs
@@ -0,0 +1,13 @@
+﻿using Unity.Entities;
+using UnityEngine;
+
+namespace TwoStickClassicExample
+{
+    public class PlayerDamageSystem : ComponentSystem
+    {
+        protected override void OnUpdate()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
