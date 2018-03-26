4190dab8f5504b46d1783ebad96acb11e47b2ab8
Sean Moakes <seanmoakes@hotmail.com>
2018-03-26 17:23:17 +0100

    Delete Behaviour

--- Assets/GameCode/Components/MoveSpeed.cs
+++ Assets/GameCode/Components/MoveSpeed.cs
@@ -5,18 +5,8 @@ using UnityEngine;
 
 namespace TwoStickClassicExample
 {
-    [RequireComponent(typeof(Position2D))]
-    [RequireComponent(typeof(Heading2D))]
     public class MoveSpeed : MonoBehaviour
     {
-
         public float Value;
-        
-        void Update ()
-        {
-            //var transform = GetComponent<Transform2D>();
-            //transform.Position += transform.Heading * Speed * Time.deltaTime;
-            //GetComponent<Position2D>().Value += GetComponent<Heading2D>().Value * Value * Time.deltaTime;
-        }
     }
 }
