3b4d0c2fde89c27106b1d35c44d864a520f3c9d9
Sean Moakes <seanmoakes@hotmail.com>
2018-03-26 18:12:33 +0100

    Create RemoveDeadSystem

---
+++ Assets/GameCode/RemoveDeadSystem.cs
@@ -0,0 +1,13 @@
+﻿using Unity.Entities;
+using UnityEngine;
+
+namespace TwoStickClassicExample
+{
+    public class RemoveDeadSystem : ComponentSystem
+    {
+        protected override void OnUpdate()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
