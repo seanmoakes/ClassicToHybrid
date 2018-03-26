98f03b8efcb7616fdca58670dc32bd8ba269343d
Sean Moakes <seanmoakes@hotmail.com>
2018-03-26 17:45:21 +0100

    Set up ComponentGroups

--- Assets/GameCode/ShotDestroySystem.cs
+++ Assets/GameCode/ShotDestroySystem.cs
@@ -1,10 +1,27 @@
-﻿using Unity.Entities;
+﻿using Unity.Collections;
+using Unity.Entities;
+using Unity.Transforms2D;
 using UnityEngine;
 
 namespace TwoStickClassicExample
 {
+    [UpdateAfter(typeof(ShotSpawnSystem))]
+    [UpdateAfter(typeof(MoveForward2DSystem))]
     public class ShotDestroySystem : ComponentSystem
     {
+        struct Data
+        {
+            public Shot Shot;
+        }
+
+        struct PlayerCheck
+        {
+            public int Length;
+            [ReadOnly] public ComponentArray<PlayerInput> PlayerInput;
+        }
+
+        [Inject] private PlayerCheck m_PlayerCheck;
+
         protected override void OnUpdate()
         {
             throw new System.NotImplementedException();
