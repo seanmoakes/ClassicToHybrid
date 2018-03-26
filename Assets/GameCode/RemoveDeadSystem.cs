8d9a51520502e2e1e8e32ef3e91294844482d561
Sean Moakes <seanmoakes@hotmail.com>
2018-03-26 18:15:33 +0100

    Create and inject component groups for all entities with a Health Component, and with a PlayerInput component.

--- Assets/GameCode/RemoveDeadSystem.cs
+++ Assets/GameCode/RemoveDeadSystem.cs
@@ -1,10 +1,27 @@
-﻿using Unity.Entities;
+﻿using Unity.Collections;
+using Unity.Entities;
 using UnityEngine;
 
 namespace TwoStickClassicExample
 {
     public class RemoveDeadSystem : ComponentSystem
     {
+        public struct Entities
+        {
+            public int Length;
+            public GameObjectArray gameObjects;
+            public ComponentArray<Health> healths;
+        }
+
+        struct PlayerCheck
+        {
+            public int Length;
+            [ReadOnly] public ComponentArray<PlayerInput> PlayerInput;
+        }
+
+        [Inject] private PlayerCheck m_PlayerCheck;
+        [Inject] private Entities entities;
+
         protected override void OnUpdate()
         {
             throw new System.NotImplementedException();
