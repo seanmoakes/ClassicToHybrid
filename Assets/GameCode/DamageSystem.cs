36d9f1cadf87671ea2c68384822a997003a290eb
Sean Moakes <seanmoakes@hotmail.com>
2018-03-26 19:05:44 +0100

    Create and inject into component groups for ReceiverData and ShotData

--- Assets/GameCode/DamageSystem.cs
+++ Assets/GameCode/DamageSystem.cs
@@ -5,6 +5,26 @@ namespace TwoStickClassicExample
 {
     public class PlayerDamageSystem : ComponentSystem
     {
+
+        public struct ReceiverData
+        {
+            public int Length;
+            public ComponentArray<Health> Health;
+            public ComponentArray<Faction> Faction;
+            public ComponentArray<Position2D> Position;
+        }
+
+        [Inject] ReceiverData m_Receivers;
+
+        public struct ShotData
+        {
+            public int Length;
+            public ComponentArray<Shot> Shot;
+            public ComponentArray<Position2D> Position;
+            public ComponentArray<Faction> Faction;
+        }
+        [Inject] ShotData m_Shots;
+
         protected override void OnUpdate()
         {
             throw new System.NotImplementedException();
