1adf5ca9c7c1953cb3f0d39ae62e79be4fbe94a7
Sean Moakes <seanmoakes@hotmail.com>
2018-03-26 18:41:25 +0100

    Create and inject into component groups for entities with an EnemyShootState component, and entities with a PlayerInput Component.

--- Assets/GameCode/EnemyShootSystem.cs
+++ Assets/GameCode/EnemyShootSystem.cs
@@ -5,6 +5,23 @@ namespace TwoStickClassicExample
 {
     public class EnemyShootSystem : ComponentSystem
     {
+        public struct Data
+        {
+            public int Length;
+            public ComponentArray<Position2D> Position;
+            public ComponentArray<EnemyShootState> ShootState;
+        }
+
+        [Inject] private Data m_Data;
+
+        public struct PlayerData
+        {
+            public int Length;
+            public ComponentArray<Position2D> Position;
+            public ComponentArray<PlayerInput> PlayerInput;
+        }
+
+        [Inject] private PlayerData m_Player;
 
         protected override void OnUpdate()
         {
